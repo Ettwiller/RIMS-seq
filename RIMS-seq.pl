0e56ad5a42af19fb23e2fd87127009769294a506
Laurence Ettwiller <ettwiller@neb.com>
2021-01-19 15:22:36 -0500

    Update RIMS-seq.pl

--- RIMS-seq.pl
+++ RIMS-seq.pl
@@ -1,27 +1,46 @@
 use strict;
+use Getopt::Long qw(GetOptions);
+use File::Temp qw(tempfile);
 use Cwd;
+use File::Spec;
 
 
-my $DIR = getcwd;
-#my $path_to_program ="/mnt/home/ettwiller/laurence/projects/5mC_detection_by_damage/programs/";
+my($vol,$dir,$file) = File::Spec->splitpath($0);
 
-my $fq1 = $ARGV[0];
-my $fq2 = $ARGV[1];
-my $path_to_program = $ARGV[2];
+my $script_dir = $dir;
+print STDERR "$script_dir\n";
+
+my $error_sentence = "\n\nUSAGE : perl $0 --fq1 fastq1.fq --fq2 fastq2.fq\n with fastq1.fq and fastq2.fq being the first and second paired-end read files respectively\n\n";
+
+# declare the options upfront :
+my $fq1;
+my $fq2;
+
+#get all options :
+GetOptions (    "fq1=s" => \$fq1,    # the Read1 file 
+		"fq2=s" => \$fq2 # the read 2 file
+    ) or die $error_sentence;
+
+#=================================
+#if something went wrong in getting the option, notify :
+if (!$fq1 ||!$fq2) {die $error_sentence}
+#=================================
+
 my $phred = 35;
 
-#my @fastq1 = <reads/*val_1.fq>;
-#my @fastq2 = <reads/*val_2.fq>;
+
+
+
+
 #programs :
 
-my $mapping_program = $path_to_program."mapping.pl";
-my $mpileup_program = $path_to_program."split_mapped_reads.pl";
-my $motif_program = $path_to_program."get_motif_all.pl";
+my $mapping_program = $script_dir."/mapping.pl";
+my $mpileup_program = $script_dir."/split_mapped_reads.pl";
+my $motif_program = $script_dir."/get_motif_all.pl";
 
-#my $assembly_program  = "/home/ettwiller/exe/SPAdes-3.13.0-Linux/bin/spades.py";
 
 #create the necessary directory
-my $mapping_dir = "mapping"; my $mpileup_dir = "mpileup"; my $motif_dir = "motif_test4";my $info_dir = "info"; my $assembly_dir = "assembly";  my $context_dir = "context";
+my $mapping_dir = "mapping"; my $mpileup_dir = "mpileup"; my $motif_dir = "motif";; my $assembly_dir = "assembly";
 if ( ! -d $mapping_dir){system("mkdir $mapping_dir");}
 if ( ! -d $mpileup_dir){system("mkdir $mpileup_dir");}
 if ( ! -d $motif_dir){system("mkdir $motif_dir");}
@@ -50,9 +69,9 @@ my $c1 = "perl $mapping_program --fq1 $fq1 --fq2 $fq2 --genome $genome --out $ba
 my $c2 = "perl $mpileup_program -bam $bam -genome $genome -mpileup1 $mpileup1 -mpileup2 $mpileup2";
 my $c3 = "perl $motif_program --mpileup1 $mpileup1 --mpileup2 $mpileup2 --qualityscore $phred --outdir $motif_dir --genome $genome --flank 7\n";
 
-system($c0);
-system($c01);
-system($c02);
+#system($c0);
+#system($c01);
+#system($c02);
 system($c1);
 system($c2);
 system($c3);
